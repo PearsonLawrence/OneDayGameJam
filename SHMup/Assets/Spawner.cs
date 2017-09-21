029b1a8d6fbaba4bdec60ad83f4445fa0250c683
Kobey Myhre <s179139@students.aie.edu.au>
2017-09-21 16:44:16 -0700

    New SPawner

--- SHMup/Assets/Spawner.cs
+++ SHMup/Assets/Spawner.cs
@@ -9,6 +9,8 @@ public class Spawner : MonoBehaviour {
     public float timer;
     private float startTime;
     public int size;
+    public int zRange;
+    public int xRange;
 	// Use this for initialization
 	void Start () {
         startTime = timer;
@@ -21,11 +23,13 @@ public class Spawner : MonoBehaviour {
         var SpawnBaby = Enemies[idx];
         for(int i = 0; i < count[idx]; i++)
         {
-            int x = Random.Range(-10, 10);
-            int z = Random.Range(-10, 10);
+            int x = Random.Range(0, xRange);
+            int z = Random.Range(0, zRange);
             SpawnBaby.transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
             Instantiate(SpawnBaby);
+            
         }
+        count[idx]++;
     }
 
 	// Update is called once per frame
